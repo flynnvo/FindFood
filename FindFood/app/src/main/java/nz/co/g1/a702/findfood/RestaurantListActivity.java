0712b2bcf9d496c4bca883938cc4b0b7cf63e588
Flynn van Os <oxappsdev@gmail.com>
2018-03-31 15:58:54 +1300

    Initial commit

---
+++ FindFood/app/src/main/java/nz/co/g1/a702/findfood/RestaurantListActivity.java
@@ -0,0 +1,13 @@
+package nz.co.g1.a702.findfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list);
+    }
+}
