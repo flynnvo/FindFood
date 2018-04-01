2637ae790ad71bebf0da5fa8904a2d08c48169b7
mohan-cao <mohancao@yahoo.com.au>
2018-04-01 19:48:18 +1200

    Fix gitignore

---
+++ FindFood/app/src/main/gen/nz/co/g1/a702/findfood/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package nz.co.g1.a702.findfood;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
