6521ff65536db2507b1e18337d7a2887f89463b8
Flynn van Os <oxappsdev@gmail.com>
2018-04-02 21:00:00 +1200

    Use location to load restaurants

--- FindFood/app/src/main/java/nz/co/g1/a702/findfood/RestaurantListViewModel.java
+++ FindFood/app/src/main/java/nz/co/g1/a702/findfood/RestaurantListViewModel.java
@@ -1,21 +1,36 @@
 package nz.co.g1.a702.findfood;
 
-import android.arch.lifecycle.ViewModel;
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 
 import java.util.List;
 
-import io.reactivex.Single;
+import io.reactivex.Maybe;
+import io.reactivex.schedulers.Schedulers;
+import nz.co.g1.a702.findfood.location.LocationRepository;
 import nz.co.g1.a702.findfood.placesapi.PlacesService;
 import nz.co.g1.a702.findfood.placesapi.Restaurant;
 
-public class RestaurantListViewModel extends ViewModel {
+public class RestaurantListViewModel extends AndroidViewModel {
     private PlacesService placesService = new PlacesService();
+    private LocationRepository locationRepository;
 
-    public Single<List<Restaurant>> getRestaurants(double latitude, double longitude) {
+    public RestaurantListViewModel(Application context) {
+        super(context);
+        locationRepository = new LocationRepository(context);
+    }
+
+    @SuppressLint("MissingPermission")
+    public Maybe<List<Restaurant>> getRestaurants() {
         String apiKey = BuildConfig.GOOGLE_API_KEY;
-        latitude = -36.848445;
-        longitude = 174.762182;
-        return placesService.getRestaurants(latitude, longitude, apiKey);
+
+        return locationRepository
+                .getLocation()
+                .observeOn(Schedulers.io())
+                .flatMapSingleElement(location -> placesService.getRestaurants(location.getLatitude(), location.getLongitude(), apiKey));
+
+
     }
 
 }
