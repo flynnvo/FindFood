01f282e103def8b211c4fa31f01616b75e348ffe
Flynn van Os <flynnv.os@gmail.com>
2018-04-08 15:55:20 +1200

    Add views for loading notes

--- FindFood/app/src/main/java/nz/co/g1/a702/findfood/restaurantdetail/RestaurantDetailActivity.java
+++ FindFood/app/src/main/java/nz/co/g1/a702/findfood/restaurantdetail/RestaurantDetailActivity.java
@@ -1,13 +1,12 @@
 package nz.co.g1.a702.findfood.restaurantdetail;
 
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,11 +21,18 @@ import static nz.co.g1.a702.findfood.RestaurantListActivity.EXTRA_RESTAURANT_NAM
 import static nz.co.g1.a702.findfood.RestaurantListActivity.EXTRA_RESTAURANT_PHOTO_URL;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
+    private ImageView appBarBackground;
+    private View appBarScrim;
+    private TextView notesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+
+        appBarBackground = findViewById(R.id.detail_app_bar_background);
+        appBarScrim = findViewById(R.id.detail_app_bar_scrim);
+        notesView = findViewById(R.id.detail_notes_view);
 
         String restaurantName = getIntent().getStringExtra(EXTRA_RESTAURANT_NAME);
         String restaurantAddress = getIntent().getStringExtra(EXTRA_RESTAURANT_ADDRESS);
@@ -40,24 +46,30 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ImageView appBarBackground = findViewById(R.id.detail_app_bar_background);
+        loadAppbarImage(photoUrl);
+        loadNotes();
+    }
 
+    private void loadAppbarImage(String photoUrl) {
         if (photoUrl != null && !photoUrl.isEmpty()) {
             Glide.with(this)
                     .load(photoUrl)
                     .into(new SimpleTarget<Drawable>() {
                         @Override
                         public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                            appBarBackground.setBackgroundDrawable(makeGrayscale(resource));
+                            appBarBackground.setBackgroundDrawable(resource);
+                            appBarScrim.setVisibility(View.VISIBLE);
                         }
                     });
         }
+
     }
 
-    private Drawable makeGrayscale(Drawable drawable) {
-        ColorMatrix colorMatrix = new ColorMatrix();
-        colorMatrix.setSaturation(0);
-        drawable.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
-        return drawable;
+    private void loadNotes() {
+        // TODO: Implement
+    }
+
+    public void editNotes(View view) {
+        // TODO: Implement
     }
 }
