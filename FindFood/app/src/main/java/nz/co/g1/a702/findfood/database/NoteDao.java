680fbab2b683d2ed60b6579c5b21a788b64cc8f3
Terran <terrankroft@gmail.com>
2018-04-28 13:13:26 +1200

    ONE LINE

--- FindFood/app/src/main/java/nz/co/g1/a702/findfood/database/NoteDao.java
+++ FindFood/app/src/main/java/nz/co/g1/a702/findfood/database/NoteDao.java
@@ -1,28 +1,13 @@
-package nz.co.g1.a702.findfood.database;
-
-import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.OnConflictStrategy;
-import android.arch.persistence.room.Query;
-import java.util.List;
-import io.reactivex.Flowable;
-
+package nz.co.g1.a702.findfood.database;import android.arch.persistence.room.Dao;import android.arch.persistence.room.Delete;import android.arch.persistence.room.Insert;import android.arch.persistence.room.OnConflictStrategy;import android.arch.persistence.room.Query;import java.util.List;import io.reactivex.Flowable;
 @Dao
 public interface NoteDao {
-
-    @Query("SELECT * FROM note")
-    Flowable<List<RestaurantNote>> getAll();
-
-    @Query("SELECT * FROM note WHERE placesId = :restaurantId")
-    Flowable<List<RestaurantNote>> getNoteById(String restaurantId);
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insert(RestaurantNote restaurantNote);
-
-    @Insert
-    void insertAll(RestaurantNote... restaurantNotes);
-
-    @Delete
-    void delete(RestaurantNote restaurantNote);
-}
+@Query("SELECT * FROM note")
+Flowable<List<RestaurantNote>> getAll();
+@Query("SELECT * FROM note WHERE placesId = :restaurantId")
+Flowable<List<RestaurantNote>> getNoteById(String restaurantId);
+@Insert(onConflict = OnConflictStrategy.REPLACE)
+void insert(RestaurantNote restaurantNote);
+@Insert
+void insertAll(RestaurantNote... restaurantNotes);
+@Delete
+void delete(RestaurantNote restaurantNote);}
